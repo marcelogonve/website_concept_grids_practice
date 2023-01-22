f3f16bdecc85bbb069942017f5c40e6fdb47ec20
Marcelo Vega <marcelogonve@gmail.com>
2023-01-22 16:14:01 -0300

    Commit inicial de los ficheros. Sin cambios previos más que lo desarrollado hasta ahora

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+(function() {
+    const sliders = [...document.querySelectorAll('.testimony-body')];
+    const buttonAfter = document.querySelector('#after');
+    const buttonBefore = document.querySelector('#before');
+    let value;
+
+    buttonAfter.addEventListener('click', () => {
+        changePosition(1);
+    });
+
+    buttonBefore.addEventListener('click', () => {
+        changePosition(-1);
+    });
+
+    const changePosition = (add) => {
+        const currentTestimony = document.querySelector('.testimony-body--show').dataset.id;
+        value = Number(currentTestimony);
+        value += add;
+
+        sliders[Number(currentTestimony)-1].classList.remove('testimony-body--show');
+        
+        if(value === sliders.length + 1 || value === 0) {
+            value = value === 0 ? sliders.length : 1;
+        }
+
+        sliders[value-1].classList.add('testimony-body--show');
+    }
+
+})();
